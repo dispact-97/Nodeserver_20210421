744db6ddb3767015cd3d5e89106653c3ccf373c8
shin <tlsdbswp97@naver.com>
2021-04-21 12:45:25 +0900

    [bug] My Router.js:8 => sBody is not defined.js

--- MyRouter.js
+++ MyRouter.js
@@ -1,15 +1,15 @@
-function route(pathname, handle, res){
-    console.log('Routing request for' + pathname);
-    if (typeof handle[pathname] === 'function'){
-        handle[pathname](res);
+  
+function route(pathname, handle, res) {
+    console.log('Routing request for ' + pathname);
+    if (typeof handle[pathname] === 'function') {
+      handle[pathname](res);
+    } else {
+      console.log('No handler for ' + pathname);
+      let body = '404 Not Found';
+      res.writeHead(404, { 'Content-Type': 'text/html' });
+      res.write(body);
+      res.end();
     }
-    else{
-        console.log('No handler for' + pathname);
-        let sBody = '404 Not Found';
-        res.writeHead(200, {'Content-Type': 'text/html'});
-        res.write(body);
-        res.end();
-    }
-}
-
-exports.route = route;
+  }
+  
+  exports.route = route;
--- Myserver.js
+++ Myserver.js
@@ -3,20 +3,18 @@ const url = require('url');
 
 const hostname = 'localhost';
 const port = 8000;
-const baseUrl = 'http://' + hostname + ":" + port;
+const baseUrl = 'http://' + hostname + ':' + port;
 
 function start(route, handle) {
-    function onRequest(req, res){
-        console.log('Request receive.');
-        pathname = url.URL(req.url. baseUrl).pathname;
-        route(pathname, handle, res);
-    }
+  function onRequest(req, res) {
+    console.log('Request receive.');
+    pathname = new url.URL(req.url, baseUrl).pathname;
+    route(pathname, handle, res);
+  }
 
-    
-
-    server = http.createServer(onRequest);
-    server.listen(port, hostname);
-    console.log('Server is running at ' + baseUrl);
+  server = http.createServer(onRequest);
+  server.listen(port, hostname);
+  console.log('Server is running at ' + baseUrl);
 }
 
 exports.start = start;
